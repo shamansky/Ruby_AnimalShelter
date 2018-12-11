0bc61165a4ed1ca990396f36a461b1a3f4be1189
George Ciurescu <shubst3r@gmail.com>
2018-12-11 11:52:22 +0000

    17th commit

--- controllers/owner_controller.rb
+++ controllers/owner_controller.rb
@@ -6,6 +6,16 @@ get '/owners' do
   erb(:"owners/index")
 end
 
+get '/owners/new' do
+  @owners = Owner.all
+  erb(:"owners/new")
+end
+
+post '/owners' do
+  Owner.new(params).save
+  redirect to '/owners'
+end
+
 get '/owners/:id' do
   @owner = Owner.find(params['id'])
   @pets = @owner.pets()
